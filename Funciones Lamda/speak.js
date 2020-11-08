f5193cd069d6b26ea1e2d48f517d7a6c4a46ad27
cristianncaste18 <cristian6cas@hotmail.es>
2020-11-07 21:37:20 -0600

    Funcionalidad Polly terminada

--- Funciones Lamda/speak.js
+++ Funciones Lamda/speak.js
@@ -5,35 +5,49 @@ const bucket_name = 'prou-mp3-polly'
     
 exports.handler =  async (event, context) => {
     const data = event;
-    const params = {
-        OutputFormat: "mp3", 
-        Text: data.text, 
-        TextType: "text", 
-        VoiceId: data.voice
-    };
-    polly.synthesizeSpeech(params).on('success', function(response){
-        const data = response.data
-        /*
-        data = {
+    const objectKey = context.awsRequestId
+    // send request to Polly
+    const  rpo = await new Promise((resolve, reject) => {
+        const params = {
+            OutputFormat: "mp3", 
+            Text: data.text, 
+            TextType: "text", 
+            VoiceId: data.voice,
+            LanguageCode: data.langcode
+        };
+         polly.synthesizeSpeech(params,function(err, data) {
+           if (err) { console.log(err, err.stack); reject(err) }// an error occurred
+           else     { console.log('Polly request successful'); resolve(data)         }// successful response
+           /*
+           data = {
             AudioStream: <Binary String>, 
             ContentType: "audio/mpeg", 
             RequestCharacters: 37
-        }*/
+           }
+           */
+        }); 
+    })
+    // save mp3 from Polly in S3
+    const rs3 = await new Promise((resolve, reject) => {
         const params = {
             Bucket: bucket_name,
-            Key: 'test' + '.mp3', // File name you want to save as in S3,
-            Body: data.AudioStream,
-            ContentType: data.ContentType
+            Key: objectKey + '.mp3',
+            Body: rpo.AudioStream,
+            ACL:'public-read',
+            ContentType: rpo.ContentType
         }
-        s3.putObject(params)
-            .on('success', function(response) {
-                console.log('S3 Put Success!')
-            })
-            .on('complete', function(response){
-                console.log('S3 Put Complete!')
-            })
-        
-    });
-  
-    return context.logStreamName
+        s3.putObject(params, (err, data) => {
+            if (err) { console.log(err, err.stack); reject(err) }// an error occurred
+            else     { console.log('S3 Put object successful!'); resolve(data)         }// successful response
+        })
+    })
+    // getUrl from S3
+    const params = { Bucket: bucket_name, Key: objectKey + '.mp3' };
+    const url = s3.getSignedUrl('getObject', params);
+    // send response
+    const response = {
+        statusCode: 200,
+        body: { url }
+    }
+    return response
 };
